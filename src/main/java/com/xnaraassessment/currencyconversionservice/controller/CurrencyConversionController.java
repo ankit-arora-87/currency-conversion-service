ce9f6abdf6eab6a3d22fdc07753ac178ff8954d5
Ankit Arora <annkit87@gmail.com>
2023-03-08 12:21:37 +0800

    Applied request validations & update API endpoint documentation details

--- src/main/java/com/xnaraassessment/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/xnaraassessment/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,11 +3,18 @@ package com.xnaraassessment.currencyconversionservice.controller;
 import com.xnaraassessment.currencyconversionservice.request.ConversionRequest;
 import com.xnaraassessment.currencyconversionservice.response.ConversionResponse;
 import com.xnaraassessment.currencyconversionservice.service.ConversionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/currencyConversion/")
@@ -17,8 +24,15 @@ public class CurrencyConversionController {
     private ConversionService conversionService;
 
 
-    @PostMapping(name = "convert")
-    public ConversionResponse convert(@RequestBody ConversionRequest conversionRequest){
+    @Operation(summary = "Convert Currency", description = "Convert Currency from source currency code to target currency code", tags = { "currency-conversion" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful conversion", content = @Content(schema = @Schema(implementation = ConversionResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+
+    })
+    @PostMapping(value = "convert", consumes = { "application/json" }, produces = { "application/json" }, name = "convert")
+    public ConversionResponse convert(@Valid @RequestBody ConversionRequest conversionRequest){
 
         return conversionService.convert(conversionRequest);
     }
--- src/main/java/com/xnaraassessment/currencyconversionservice/request/ConversionRequest.java
+++ src/main/java/com/xnaraassessment/currencyconversionservice/request/ConversionRequest.java
@@ -8,18 +8,20 @@ import lombok.NonNull;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConversionRequest {
 
-    @NonNull
+    @NotBlank(message = "Please provide curr_code")
     private String curr_code;
-    @NotNull
+    @NotNull(message = "Please provide amount")
+    @Positive(message = "Invalid amount. Amount must be greater than 0.0")
     private Double amount;
-    @NotBlank
+    @NotBlank(message = "Please provide language")
     private String language;
-    @NotEmpty
+    @NotBlank(message = "Please provide target_code")
     private String target_code;
 }
